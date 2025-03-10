dee21150bd3bd5c475d58bff41d408fa6097a8d0
jbgpt <134644850+jbgpt@users.noreply.github.com>
2025-03-10 11:49:15 +0800

    Update update.sh

--- mosdns/ai/update.sh
+++ mosdns/ai/update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+touch /var/mosdns/query.log
 # 下载 geoip.dat 文件
 curl -o /var/mosdns/geoip.dat https://cdn.jsdelivr.net/gh/Loyalsoldier/geoip@release/geoip.dat
 
@@ -7,6 +7,7 @@ curl -o /var/mosdns/geoip.dat https://cdn.jsdelivr.net/gh/Loyalsoldier/geoip@rel
 curl -o /var/mosdns/geosite.dat https://gh-proxy.com///https://github.com/Loyalsoldier/domain-list-custom/releases/latest/download/geosite.dat
 # 下载 private.dat 文件
 curl -o /var/mosdns/geo_private.dat https://cdn.jsdelivr.net/gh/Loyalsoldier/geoip@release/private.dat
-
+# 下载gfw.txt
+# 下载!cn
 # 重新加载 mosdns 配置
 systemctl restart mosdns
